78c73f4a1a0594b2e75ab6bbf145469b412d4696
Skywalker8510 <91810623+Skywalker8510@users.noreply.github.com>
2025-03-24 12:10:50 -0500

    better variable naming

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,19 @@ use std::path::Path;
 
 #[tokio::main]
 async fn main() {
+    //TODO Change this to a TOML file instead of using JSON.
+    //**********************************************************************
     let mut file = File::open("./src/default.json").unwrap();
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
 
     let v: Value = serde_json::from_str(&data).unwrap();
     let folder_path = v["target_path"].as_str().unwrap();
+    //**********************************************************************
 
     for file in read_dir(folder_path).unwrap() {
         let jar_path = file.unwrap().path();
-        let fabricmod_id = get_id(jar_path.clone()).expect("No Id Found");
+        let fabricmod_id = get_fabric_id(jar_path.clone()).expect("No Id Found");
 
         let client = reqwest::Client::new();
         let search_result = client
@@ -29,12 +32,12 @@ async fn main() {
             .await
             .unwrap();
 
-        let v: Value = serde_json::from_str(&search_result).unwrap();
+        let api_search_results: Value = serde_json::from_str(&search_result).unwrap();
 
-        let project_result = client
+        let project_search = client
             .get(format!(
                 "https://api.modrinth.com/v2/project/{}",
-                v["hits"][0]["project_id"].as_str().unwrap()
+                api_search_results["hits"][0]["project_id"].as_str().unwrap()
             ))
             .send()
             .await
@@ -43,11 +46,11 @@ async fn main() {
             .await
             .unwrap();
 
-        let v: Value = serde_json::from_str(&project_result).unwrap();
+        let api_project_results: Value = serde_json::from_str(&project_search).unwrap();
 
-        let version_id_array = v["versions"].as_array().unwrap();
+        let version_id_array = api_project_results["versions"].as_array().unwrap();
 
-        let version_results = client
+        let version_search = client
             .get(format!(
                 "https://api.modrinth.com/v2/version/{}",
                 version_id_array[version_id_array.len() - 1]
@@ -61,28 +64,30 @@ async fn main() {
             .await
             .unwrap();
 
-        let v: Value = serde_json::from_str(&version_results).unwrap();
+        let api_version_results: Value = serde_json::from_str(&version_search).unwrap();
 
-        let url = v["files"][0]["url"].as_str().unwrap();
+        let download_url = api_version_results["files"][0]["url"].as_str().unwrap();
         let download_path = format!(
             "./{}{}",
             folder_path,
-            v["files"][0]["filename"].as_str().unwrap()
+            api_version_results["files"][0]["filename"]
+                .as_str()
+                .unwrap()
         );
 
-        download_files(url, &download_path).await.unwrap();
+        download_files(download_url, &download_path).await.unwrap();
     }
 }
 
-fn get_id<P: AsRef<Path>>(path: P) -> Option<String> {
+fn get_fabric_id<P: AsRef<Path>>(path: P) -> Option<String> {
     let file = File::open(path).unwrap();
 
-    let mut archive = zip::ZipArchive::new(file).unwrap();
+    let mut jar = zip::ZipArchive::new(file).unwrap();
 
-    let file = archive.by_name("fabric.mod.json").ok()?;
-    let v: Value = serde_json::from_reader(file).unwrap();
+    let file = jar.by_name("fabric.mod.json").ok()?;
+    let fabricmod_json: Value = serde_json::from_reader(file).unwrap();
 
-    Some(v["id"].to_string())
+    Some(fabricmod_json["id"].to_string())
 }
 
 async fn download_files(url: &str, path: &str) -> Result<(), Box<dyn std::error::Error>> {
