9b1aa0b2c786f690fcf7ec556dc6ea13ea88e4ae
Skywalker8510 <91810623+Skywalker8510@users.noreply.github.com>
2025-04-09 15:20:59 -0500

    Moved api requests to functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use futures_util::StreamExt;
-use reqwest::get;
+use reqwest::{Client, get};
 use serde_json::Value;
 use std::fs::{File, read_dir};
 use std::io::{Read, Write};
@@ -21,58 +21,16 @@ async fn main() {
         let jar_path = file.unwrap().path();
         let fabricmod_id = get_fabric_id(jar_path.clone()).expect("No Id Found");
 
-        let client = reqwest::Client::new();
-        let search_result = client
-            .get("https://api.modrinth.com/v2/search")
-            .query(&[("query", fabricmod_id)])
-            .send()
-            .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap();
+        let client = Client::new();
+        let api_search_result = get_api_search_result(client.clone(), fabricmod_id).await.unwrap();
+        let api_project_result = get_api_project_result(client.clone(), api_search_result).await.unwrap();
+        let api_version_result = get_api_version_result(client, api_project_result).await.unwrap();
 
-        let api_search_results: Value = serde_json::from_str(&search_result).unwrap();
-
-        let project_search = client
-            .get(format!(
-                "https://api.modrinth.com/v2/project/{}",
-                api_search_results["hits"][0]["project_id"].as_str().unwrap()
-            ))
-            .send()
-            .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap();
-
-        let api_project_results: Value = serde_json::from_str(&project_search).unwrap();
-
-        let version_id_array = api_project_results["versions"].as_array().unwrap();
-
-        let version_search = client
-            .get(format!(
-                "https://api.modrinth.com/v2/version/{}",
-                version_id_array[version_id_array.len() - 1]
-                    .as_str()
-                    .unwrap()
-            ))
-            .send()
-            .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap();
-
-        let api_version_results: Value = serde_json::from_str(&version_search).unwrap();
-
-        let download_url = api_version_results["files"][0]["url"].as_str().unwrap();
+        let download_url = api_version_result["files"][0]["url"].as_str().unwrap();
         let download_path = format!(
             "./{}{}",
             folder_path,
-            api_version_results["files"][0]["filename"]
-                .as_str()
-                .unwrap()
+            api_version_result["files"][0]["filename"].as_str().unwrap()
         );
 
         download_files(download_url, &download_path).await.unwrap();
@@ -101,4 +59,45 @@ async fn download_files(url: &str, path: &str) -> Result<(), Box<dyn std::error:
 
     file.flush()?;
     Ok(())
+}
+
+async fn get_api_search_result(
+    client: Client,
+    fabricmod_id: String,
+) -> Result<Value, Box<dyn std::error::Error>> {
+    let search_result = client.get("https://api.modrinth.com/v2/search").query(&[("query", fabricmod_id)]).send().await?.text().await?;
+    let api_search_results: Value = serde_json::from_str(&search_result)?;
+    
+    Ok(api_search_results)
+}
+
+async fn get_api_project_result(
+    client: Client,
+    api_search_results: Value,
+) -> Result<Value, Box<dyn std::error::Error>> {
+    let project_search = client
+        .get(format!(
+            "https://api.modrinth.com/v2/project/{}",
+            api_search_results["hits"][0]["project_id"]
+                .as_str()
+                .unwrap()
+        ))
+        .send()
+        .await?
+        .text()
+        .await?;
+    let api_project_results: Value = serde_json::from_str(&project_search)?;
+    
+    Ok(api_project_results)
+}
+
+async fn get_api_version_result(
+    client: Client,
+    api_project_results: Value,
+) -> Result<Value, Box<dyn std::error::Error>> {
+    let version_id_array = api_project_results["versions"].as_array().unwrap();
+    let version_search = client.get(format!("https://api.modrinth.com/v2/version/{}", version_id_array[version_id_array.len() - 1].as_str().unwrap())).send().await?.text().await?;
+    let api_version_results: Value = serde_json::from_str(&version_search)?;
+    
+    Ok(api_version_results)
 }
