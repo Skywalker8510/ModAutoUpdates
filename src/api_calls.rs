a3939202535db2aec6145a75c8e3449ffc151fa6
Skywalker8510 <91810623+Skywalker8510@users.noreply.github.com>
2025-05-22 17:01:07 -0500

    refactor file structure

---
+++ src/api_calls.rs
@@ -0,0 +1,64 @@
+use reqwest::Client;
+use serde_json::Value;
+
+pub async fn get_api_search_result(
+    client: Client,
+    fabricmod_id: String,
+    config: Value,
+) -> Result<Value, Box<dyn std::error::Error>> {
+    let search_result = client
+        .get("https://api.modrinth.com/v2/search")
+        .query(&[
+            ("query", fabricmod_id),
+            (
+                "facets",
+                format!(
+                    "[[\"categories:{}\"],[\"versions:{}\"]]",
+                    config["loader_version"], config["server_version"]
+                ),
+            ),
+        ])
+        .send()
+        .await?
+        .text()
+        .await?;
+    let api_search_results: Value = serde_json::from_str(&search_result)?;
+
+    Ok(api_search_results)
+}
+
+pub async fn get_api_project_result(
+    client: Client,
+    project_id: String,
+) -> Result<Value, Box<dyn std::error::Error>> {
+    let project_search = client
+        .get(format!(
+            "https://api.modrinth.com/v2/project/{}",
+            project_id
+        ))
+        .send()
+        .await?
+        .text()
+        .await?;
+    let api_project_results: Value = serde_json::from_str(&project_search)?;
+
+    Ok(api_project_results)
+}
+
+pub async fn get_api_version_result(
+    client: Client,
+    version_id: &Value,
+) -> Result<Value, Box<dyn std::error::Error>> {
+    let version_search = client
+        .get(format!(
+            "https://api.modrinth.com/v2/version/{}",
+            version_id
+        ))
+        .send()
+        .await?
+        .text()
+        .await?;
+    let api_version_results: Value = serde_json::from_str(&version_search)?;
+
+    Ok(api_version_results)
+}
